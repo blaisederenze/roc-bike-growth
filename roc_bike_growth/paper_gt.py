c9797cd652c2722b853b929033f44f0a8e8bd4ba
blaisederenze <82596077+blaisederenze@users.noreply.github.com>
2022-04-29 16:54:47 -0400

    Added Hybrid Pruning

--- roc_bike_growth/paper_gt.py
+++ roc_bike_growth/paper_gt.py
@@ -17,11 +17,10 @@ def get_poipairs_by_distance(G, pois_indices, route_factor = 0):
     poi_nodes = []
     poi_edges = []
     
-    if(True):
-        for e in G.es:
-            e["mod_weight"] = e["weight"] - e["weight"] * route_factor * int(
-                e["existing"] == True
-            )
+    for e in G.es:
+        e["mod_weight"] = e["weight"] - e["weight"] * route_factor * int(
+            e["existing"] == True
+        )
             
         
     
@@ -30,6 +29,7 @@ def get_poipairs_by_distance(G, pois_indices, route_factor = 0):
         # We'd have to implement a "get shortest paths  weighted on accident data etc."
         # could also include node weights
         poi_nodes.append(G.get_shortest_paths(v, pois_indices[c:], output="vpath", weights = "mod_weight"))
+        
         poi_edges.append(G.get_shortest_paths(v, pois_indices[c:], output="epath", weights = "mod_weight"))
     
     
@@ -37,10 +37,13 @@ def get_poipairs_by_distance(G, pois_indices, route_factor = 0):
     # Sum up weights (distances) of all paths
     poi_dist = {}
     for paths_n, paths_e in zip(poi_nodes, poi_edges):
+        
         for path_n, path_e in zip(paths_n, paths_e):
+            
             # Sum up distances of path segments from first to last node
             # would have to use distances incorporating parameters if we were to use them here.
             path_dist = sum([G.es[e]["mod_weight"] for e in path_e])
+            
             if path_dist > 0:
                 poi_dist[(path_n[0], path_n[-1])] = path_dist
 
@@ -48,6 +51,7 @@ def get_poipairs_by_distance(G, pois_indices, route_factor = 0):
     # Back to ids
     poipairs = []
     for p in temp:
+        
         poipairs.append([(G.vs[p[0][0]]["id"], G.vs[p[0][1]]["id"]), p[1]])
 
     return poipairs
@@ -55,15 +59,11 @@ def get_poipairs_by_distance(G, pois_indices, route_factor = 0):
 
 def greedy_triangulation(GT, poipairs, prune_factor=1, route_factor = 0, prune_measure="betweenness"):
     for poipair, poipair_distance in poipairs:
-        try:
-            poipair_ind = (
-                GT.vs.find(id=poipair[0]).index,
-                GT.vs.find(id=poipair[1]).index,
-            )
-        except:
-            print(poipair)
-            for v in GT.vs:
-                print(v["id"])
+        
+        poipair_ind = (
+            GT.vs.find(id=poipair[0]).index,
+            GT.vs.find(id=poipair[1]).index,
+        )
 
         if not new_edge_intersects(
             GT,
@@ -77,34 +77,49 @@ def greedy_triangulation(GT, poipairs, prune_factor=1, route_factor = 0, prune_m
             GT.add_edge(poipair_ind[0], poipair_ind[1], weight=poipair_distance)
 
     # Get the measure for pruning
-    
-    print(prune_measure)
-    
+        
     prune_quantile = prune_factor
     
+    if(prune_measure == "hybrid"):
+        
+        prune_quantile = prune_quantile*2
+        
+        if(prune_quantile > 1):
+            prune_quantile = 1.0
+        
+    
     if prune_measure == "betweenness" or prune_measure == "hybrid":
+        
+        
         BW = GT.edge_betweenness(directed = True, weights = "weight")
         qt = np.quantile(BW, 1-prune_quantile)
         sub_edges = []
+        
         for c, e in enumerate(GT.es):
+            
             if BW[c] >= qt: 
                 sub_edges.append(c)
             GT.es[c]["bw"] = BW[c]
             GT.es[c]["width"] = math.sqrt(BW[c]+1)*0.5
         # Prune
         GT = GT.subgraph_edges(sub_edges)
+        
     elif prune_measure == "closeness":
+        
         CC = GT.closeness(vertices = None, weights = "weight")
         qt = np.quantile(CC, 1-prune_quantile)
         sub_nodes = []
+        
         for c, v in enumerate(GT.vs):
+            
             if CC[c] >= qt: 
                 sub_nodes.append(c)
             GT.vs[c]["cc"] = CC[c]
+            
         GT = GT.induced_subgraph(sub_nodes) 
         
     elif prune_measure == "iter_betweenness":
-        print('here')
+        pass
     
     return GT
 
@@ -121,6 +136,7 @@ def route_node_pairs(G, GT, route_factor):
     routenodepairs = {}
     for e in GT.es:
         routenodepairs[(e.source_vertex["id"], e.target_vertex["id"])] = e["weight"]
+        
     routenodepairs = sorted(routenodepairs.items(), key=lambda x: x[1])
 
     # Do the routing
@@ -133,7 +149,9 @@ def route_node_pairs(G, GT, route_factor):
         )
 
     for poipair, poipair_distance in routenodepairs:
+        
         poipair_ind = (G.vs.find(id=poipair[0]).index, G.vs.find(id=poipair[1]).index)
+        
         sp = set(
             G.get_shortest_paths(
                 poipair_ind[0], poipair_ind[1], weights="mod_weight", output="vpath"
@@ -205,7 +223,6 @@ def gt_from_scratch(G, pois_indices, route_factor=0, prune_factor=1, prune_measu
         G_temp.es.delete(e)
     GT = copy.deepcopy(G_temp.subgraph(set(pois_indices)))
     poipairs = get_poipairs_by_distance(G, pois_indices, route_factor = route_factor)
-    # print(poipairs)
     GT = greedy_triangulation(GT, poipairs, prune_factor, prune_measure = prune_measure, route_factor = route_factor)
     GT_final = route_node_pairs(G, GT, route_factor)
     return GT_final
@@ -215,38 +232,72 @@ def gt_with_existing_full(G_base, G_existing, route_factor=0, prune_factor=1, pr
 
     G_comb_nx = gu.combine_nodes(G_base, G_existing)
     G_comb_nx = gu.combine_edges(G_comb_nx, G_existing)
-
     G_comb_nx = set_all_edge_attributes(G_existing, G_comb_nx, "existing")
-
     G_comb = ig.Graph.from_networkx(G_comb_nx)
 
     for i, v in enumerate(G_comb.vs):
         v["id"] = i
     for e in G_comb.es:
         e["weight"] = e["length"]
+        
     pois_ids = [v_index for v_index, vertex in enumerate(G_comb.vs) if vertex["poi"]]
+    
     G_gen = gt_from_scratch(G_comb, pois_ids, route_factor, prune_factor,prune_measure = prune_measure)
-    if((prune_measure == 'iter_betweenness') or (prune_measure == 'hybrid')):
-        G_gen = iterative_pruning(G_gen,G_existing, prune_factor, by_factor)
-
-    G_nx = gu.ig_to_nx(G_gen)
-    G_nx = gu.combine_nodes(G_nx, G_existing)
-    G_nx = gu.combine_edges(G_nx, G_existing)
-
-    G_nx = set_all_edge_attributes(G_existing, G_nx, "existing")
-    G_nx = set_all_edge_attributes(gu.ig_to_nx(G_gen), G_nx, "generated")
+    
+    
     
     if(prune_measure == 'iter_betweenness'):
+        
+        G_gen = iterative_pruning(G_gen,G_existing, prune_factor, by_factor)
+       
+        
+        
+    G_nx = gu.ig_to_nx(G_gen)
+    G_nx = gu.combine_nodes(G_nx, G_existing)
+    G_nx = gu.combine_edges(G_nx, G_existing)    
+
+    G_nx = set_all_edge_attributes(G_existing, G_nx, "existing")
+    
+    
+    if(prune_measure == 'hybrid'):
+        G_post = ig.Graph.from_networkx(G_nx)
+        
+        for e in G_post.es:
+            if(e['length'] == None):
+                G_post.delete_edges(e)
+    
+
+        for e in G_post.es:
+            e['weight'] = e['length']
+            e["mod_weight"] = e["weight"] - e["weight"] * route_factor * int(
+                    e["existing"] == True
+                )
+        
+        G_gen = iterative_pruning(G_post ,G_existing, prune_factor, by_factor)
+        G_nx = gu.ig_to_nx(G_gen)
+        
+    G_nx = set_all_edge_attributes(gu.ig_to_nx(G_gen), G_nx, "generated")
+    
+    
+    
+    
+    
+    
+    if((prune_measure == 'iter_betweenness') or (prune_measure == 'hybrid')):
         G_nx.remove_nodes_from(list(nx.isolates(G_nx)))
         
     return G_nx
             
 def iterative_pruning(GT, existing, prune_factor, by_factor):
     
+    
     if(by_factor == "mod"):
         #modified by factor speeding up algorithm
         #start = 20, half  = 10, 1/4 =5 1/8 = 2 1/16 = 1
-        by_factors = [40,20,10,5,2,1] 
+        by_factors = [40,20,10,5,2,1]
+        '''elif(type(by_factor) == list):
+        if(len(by_factor > 6):
+           by_factors = by_factor[0:5]'''
     else:
         by_factors =  [by_factor]*6
     
@@ -262,7 +313,6 @@ def iterative_pruning(GT, existing, prune_factor, by_factor):
             
             factor = factor/2
             bf_index+=1
-        print('factor: ' + str(by_factors[bf_index]))
         
         BW = GT.edge_betweenness(directed = True, weights = "mod_weight")
         
@@ -280,11 +330,7 @@ def iterative_pruning(GT, existing, prune_factor, by_factor):
             
             pos = list(np.where(np.array(BW) == min_value)[0])
             edges = [GT.es[i] for i in pos] + edges
-        
-        print(len(edges))
             
         GT.delete_edges(edges)
-        
-    print(len(GT.es))
-    
+            
     return GT
